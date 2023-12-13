32628eff012c410970086976cd4a7fee3aaeb822
eunyeong <feijoaplz@gmail.com>
2023-12-14 02:22:45 +0900

    feat: 페어를 매칭 하는 기능

--- src/main/kotlin/pairmatching/model/PairResult.kt
+++ src/main/kotlin/pairmatching/model/PairResult.kt
@@ -1,4 +1,22 @@
 package pairmatching.model
 
-class PairResult {
+import camp.nextstep.edu.missionutils.Randoms.shuffle
+class PairResult(course: String, level: String, mission: String) {
+    private val pairs = mutableListOf<String>()
+
+    fun makePairs(curriculum: List<String>) {
+        val randomPair = shuffle(curriculum)
+
+        for (random in randomPair) {
+            pairs.add(random)
+        }
+    }
+    
+    fun getPairs() {
+        val stringBuilder = StringBuilder()
+        for (i in pairs.indices) {
+            stringBuilder.append(pairs[i] + " : " + pairs[i+1])
+        }
+    }
 }
+
