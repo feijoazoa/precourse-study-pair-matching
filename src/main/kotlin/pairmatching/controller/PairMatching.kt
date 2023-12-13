7cd25e4717a33a2e062ec91fcadbee8e07c5d64f
eunyeong <feijoaplz@gmail.com>
2023-12-14 02:52:27 +0900

    feat: 컨트롤러 기능

--- src/main/kotlin/pairmatching/controller/PairMatching.kt
+++ src/main/kotlin/pairmatching/controller/PairMatching.kt
@@ -1,10 +1,22 @@
 package pairmatching.controller
 
+import net.bytebuddy.asm.MemberSubstitution.Substitution.ForMethodInvocation.MethodResolver.Matching
+import pairmatching.MatchingService
 import pairmatching.model.Data
+import pairmatching.model.Service
 
 class PairMatching {
     private val data = Data()
+    private val matchingService = MatchingService()
     fun start() {
         data.readFile()
+        while (true) {
+            when (matchingService.choose()) {
+                Service.FIRST.getChoice() -> matchingService.getMatching()
+                Service.SECOND.getChoice() -> matchingService.showPairs()
+                Service.THIRD.getChoice() -> matchingService.initPairs()
+                Service.QUIT.getChoice() -> break
+            }
+        }
     }
 }
